7adf4cca2014d1f84801adc5b5462bbce6f397df
kendrickf <42994705+kendrickf@users.noreply.github.com>
2019-12-10 19:48:56 +0700

    Add files via upload
    only test cases and script (code)

---
+++ Scripts/15. Menyusun Jadwal Perjalanan/Script1574936717171.groovy
@@ -0,0 +1,27 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.trafi.com/id/jakarta/route-search/Jakarta%20Special%20Capital%20Region-loc--6.17539405822754;106.827186584473/Serpong-loc--6.25347661972046;106.76432800293/now')
+
+WebUI.click(findTestObject('Object Repository/Page_Trafi Jakarta - Info KRL  TransJakarta/a_Meninggalkan sekarang'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Trafi Jakarta - Info KRL  TransJakarta/select_000510152025303540455055'), 
+    '50', true)
+
---
+++ Scripts/20. memilih transportasi dalam pemilihan alamat di kota madrid/Script1574936801721.groovy
@@ -0,0 +1,24 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.trafi.com/es/madrid/route-search/%C3%81rea%20metropolitana%20de%20Madrid%20y%20Corredor%20del%20Henares-loc-40.4221801757813;-3.69051456451416/Santiago%20Bernab%C3%A9u%20Stadium-loc-40.453052520752;-3.68754625320435/now')
+
+WebUI.click(findTestObject('Object Repository/Page_Trafi Madrid - Navigate Public Transit/div_3C2C1C101108Leave nowArrive at 1129Reco_8e5869'))
+
